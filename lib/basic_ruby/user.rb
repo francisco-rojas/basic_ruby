0b28e37623c2c7d4c0677d103cbe0bd34509da57
xjbrenesx <javierbr_1709@hotmail.com>
2015-07-11 15:47:53 -0600

    Add first_name,last_name and age for  user class

--- lib/basic_ruby/user.rb
+++ lib/basic_ruby/user.rb
@@ -1,3 +1,17 @@
-class User
+# Date: 11/07/2015
+# Description: Create a User class 
+# Developer: Javier Brenes Redondo
 
+class User
+  attr_accessor :last_Name 
+  attr_reader :age
+  attr_writer :age 
+  
+  def first_name=(value) 
+    @first_name = value
+  end 
+
+  def first_name 
+    @first_name
+  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 describe User do
+  it "sets the first name" do
+    user = User.new 
+    user.first_name = "Javier"
+    expect(user.first_name).to eq "Javier"
+  end
 
+  it "sets the last name" do
+    user = User.new 
+    user.last_Name = "Brenes"
+    expect(user.last_Name).to eq "Brenes"
+  end
+
+   it "sets the age" do
+    user = User.new 
+    user.age = 20
+    expect(user.age).to eq 20
+  end
 end
