4f030585f3cd5616b6933d3dbe1240a271f3b162
Francisco Rojas <josefcorojas@gmail.com>
2015-07-18 14:39:11 -0600

    Add User#save and User.all, User.count methods

--- lib/basic_ruby/user.rb
+++ lib/basic_ruby/user.rb
@@ -2,6 +2,7 @@ class User
   attr_accessor :last_name, :birth_date, :address
   attr_reader :age
   attr_writer :age
+  @@users = []
 
   def first_name=(value)
     @first_name = value
@@ -9,5 +10,17 @@ class User
 
   def first_name
     @first_name
+  end
+
+  def save
+    @@users << self
+  end
+
+  def self.all
+    @@users
+  end
+
+  def self.count
+    @@users.count
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,24 +1,48 @@
 require 'spec_helper'
 
 describe User do
-  it "sets the first name" do
-    user = User.new
-    user.first_name = "Francisco"
-
-    expect(user.first_name).to eq "Francisco"
+  before(:each) do
+    User.class_variable_set(:@@users, [])
   end
 
-  it "sets the last name" do
-    user = User.new
-    user.last_name = "Rojas"
+  describe 'instance methods' do
+    it "sets the first name" do
+      user = User.new
+      user.first_name = "Francisco"
 
-    expect(user.last_name).to eq "Rojas"
+      expect(user.first_name).to eq "Francisco"
+    end
+
+    it "sets the last name" do
+      user = User.new
+      user.last_name = "Rojas"
+
+      expect(user.last_name).to eq "Rojas"
+    end
+
+    it "sets the age" do
+      user = User.new
+      user.age = 27
+
+      expect(user.age).to eq 27
+    end
+
+    it 'saves the user instance into the @@users array' do
+      user = User.new
+      user.first_name = "Francisco"
+      expect(User.count).to eq 0
+      user.save
+      expect(User.count).to eq 1
+    end
   end
 
-  it "sets the age" do
-    user = User.new
-    user.age = 27
+  describe 'class methods' do
+    it 'returns an array with all users' do
+      user = User.new
+      user.first_name = "Francisco"
+      user.save
 
-    expect(user.age).to eq 27
+      expect(User.all).to eq [user]
+    end
   end
 end
