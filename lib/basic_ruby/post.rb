a83decb5a71a2a6d39b9333d02d4d6cb57d27a94
Francisco Rojas <josefcorojas@gmail.com>
2015-08-01 16:57:26 -0600

    Create Base class to hold common functionality among clases

--- lib/basic_ruby/post.rb
+++ lib/basic_ruby/post.rb
@@ -1,32 +1,14 @@
-class Post
-  attr_accessor :title, :description, :user, :comments, :id
+class Post < Base
+  attr_accessor :title, :description, :user, :comments
+
   @@posts = []
 
-  def self.count
-    @@posts.count
-  end
-
-  def self.all
+  def self.collection
     @@posts
   end
 
-  def self.find(id)
-    @@posts.find { |post| post.id == id }
-  end
-
-  def self.find_by(attribute, value)
-    return [] unless value
-    @@posts.find_all { |post| post.send(attribute) == value }
-  end
-
-  def save
-    validates_presence_of_user
-    @id = @@posts.count + 1
-    @@posts << self
-  end
-
-  def destroy
-    @@posts.delete(self)
+  def validations
+    [:validates_presence_of_user]
   end
 
   private
--- lib/basic_ruby/user.rb
+++ lib/basic_ruby/user.rb
@@ -1,38 +1,22 @@
-class User
-  attr_accessor :first_name, :last_name, :birth_date, :address, :age
-  attr_reader :id
+class User < Base
+  attr_accessor :first_name,
+                :last_name,
+                :birth_date,
+                :address,
+                :age
 
   @@users = []
 
-  def self.all
+  def self.collection
     @@users
-  end
-
-  def self.count
-    @@users.count
-  end
-
-  def self.find(id)
-    @@users.find { |user| user.id == id }
-  end
-
-  def self.find_by(attribute, value)
-    return [] unless value
-    @@users.find_all { |user| user.send(attribute) == value }
-  end
-
-  def save
-    validate_first_name
-    @id = @@users.count + 1
-    @@users << self
   end
 
   def posts
     Post.find_by(:user, self)
   end
 
-  def destroy
-    @@users.delete(self)
+  def validations
+    [:validate_first_name]
   end
 
   private
