94e32c3d2a006ccfb41f43b051cf5a7a486f97e7
Francisco Rojas <josefcorojas@gmail.com>
2015-08-08 13:11:10 -0600

    Add Comment class implementation

--- lib/basic_ruby.rb
+++ lib/basic_ruby.rb
@@ -2,6 +2,7 @@ require "basic_ruby/version"
 require "basic_ruby/base"
 require "basic_ruby/user"
 require "basic_ruby/post"
+require "basic_ruby/comment"
 
 module BasicRuby
 end
---
+++ lib/basic_ruby/comment.rb
@@ -0,0 +1,26 @@
+class Comment < Base
+  attr_accessor :user, :post, :text
+
+  @@comments = []
+
+  def self.collection
+    @@comments
+  end
+
+  def validations
+    [:validate_presence_of_user, :validate_presence_of_post]
+  end
+
+  private
+  def validate_presence_of_user
+    if user.nil? || !user.id
+      raise 'Comments must be assigned to a user'
+    end
+  end
+
+  def validate_presence_of_post
+    if post.nil? || !post.id
+      raise 'Comments must be assigned to a post'
+    end
+  end
+end
